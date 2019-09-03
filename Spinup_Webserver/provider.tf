579b9366dde6d14b0563cd2eacca4290a35a80dd
Bhardwaj Chittoor Ganesh Kumar <cgkbharadwaj@gmail.com>
2019-09-03 21:08:04 +0100

    revoke assume role change

--- Spinup_Webserver/provider.tf
+++ Spinup_Webserver/provider.tf
@@ -2,9 +2,4 @@ provider "aws" {
     access_key = "${var.AWS_ACCESS_KEY}"
     secret_key = "${var.AWS_SECRET_KEY}"
     region = "${var.AWS_REGION}"
-    assume_role {
-    role_arn     = "arn:aws:iam::736261142003:role/EC2Admin"
-    session_name = "SESSION_NAME"
-    external_id  = "EXTERNAL_ID"
-  }
 }
