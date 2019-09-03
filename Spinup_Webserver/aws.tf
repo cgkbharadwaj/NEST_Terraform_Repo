f971e1b308a46aef4c5e62d259a9401b4aca80fb
Bhardwaj Chittoor Ganesh Kumar <cgkbharadwaj@gmail.com>
2019-09-03 20:39:58 +0100

    remove duplicate provider

--- Spinup_Webserver/aws.tf
+++ Spinup_Webserver/aws.tf
@@ -1,8 +1,3 @@
-provider "aws" {
-  profile    = "default"
-  region     = "eu-west-1"
-}
-
 resource "aws_instance" "example" {
   ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
