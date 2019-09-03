e7d84ad66860c57a55eeaf19100d2f10c4adb725
Bhardwaj Chittoor Ganesh Kumar <cgkbharadwaj@gmail.com>
2019-09-04 00:53:04 +0100

    t2.xlarge

--- Spinup_Webserver/aws.tf
+++ Spinup_Webserver/aws.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "example" {
   ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
-  instance_type = "t2.micro"
+  instance_type = "t2.xlarge"
   tags = {
     Name = "testTag"
   }
