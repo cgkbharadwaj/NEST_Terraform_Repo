83a55b4668a7c770bfb1fcff9db93c858c0845a9
Bhardwaj Chittoor Ganesh Kumar <cgkbharadwaj@gmail.com>
2019-09-04 02:56:50 +0100

    key fix

--- Spinup_Webserver/aws.tf
+++ Spinup_Webserver/aws.tf
@@ -1,6 +1,6 @@
-resource aws_key_pair" "prep-key"{
+resource "aws_key_pair" "prep-key"{
 key_name = "prep-key"
-public_key = ""
+public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDdOvRissw7hFatMDPg0+MNlJhLaZDXaHynj/5+2EOBSX7OHOg1fBrw0CJ9V4N3lyJ2UJUQddtDc4uDjZoKww7q9hQsvC8RMGvIzNtQaRL+/pOlg3E3s31MK1dqsFat0MswT+iGwK+h2cpAPvE8ZfydHRYnrDxCRAyPZVrnyAbdy5SLariwRFhW3ELQVHXkYvg6NcRyCjBb2smSvDbCvZWI/fGL7xfp0TS9c6EE8jNFlqzU0Cl/pxWXnwXuxRtV7sYYykUIFpc5vmkLhF+M7pt0Ir/KyuN6RyTBEav6qF3Zy+fLETu5wwbiDglfeSR86Do5CAUdwXVAj36/uIcps8u7 root@ip-10-0-0-194"
 }
 resource "aws_instance" "example" {
   ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
